c18fb7cbc19449b3b641021df7f6d657c5c93c8e
Trevor Martin <tcmartin5000@gmail.com>
2023-08-10 02:38:35 -0400

    Scoreboard now fully functional.

---
+++ ScoreDisplayTileMap.adb
@@ -0,0 +1,2 @@
+M:ScoreDisplayTileMap
+S:G$ScoreDisplayTileMap$0_0$0({24}DA24d,SC:U),D,0,0
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <gb/gb.h>
+#include <gbdk/font.h>
 #include <stdint.h>
 #include "BackgroundTileMap.h"
 #include "BackgroundTiles1.h"
@@ -7,6 +8,69 @@
 #include "hUGEDriver.h"
 
 extern const hUGESong_t dave_bgm;
+/* First six pointers are window score digits, left to right. Second six are high score digits. */
+uint8_t *score_pointers[12];
+
+/* Reads arrays as three ints in length, adds to them increment where necessary. */
+void incrementAndDrawScore(uint8_t *scoreArray, uint8_t *highScoreArray, uint8_t increment)
+{
+    for (uint8_t i = 2; i <= 2; i--)
+    {
+        // Add the increment to the current segment.
+        scoreArray[i] += increment;
+        // Have we overflown?
+        if (scoreArray[i] > 99)
+        {
+            // If so, are we at the leftmost segment? Then max the score.
+            if (i == 0)
+            {
+                scoreArray[i] = 99;
+                scoreArray[i+1] = 99;
+                scoreArray[i+2] = 99;
+                for (uint8_t j = 0; j < 6; j++) {
+                    set_vram_byte(score_pointers[j], 0x0A);
+                }
+            }
+            // If not, then set the increment to this segment / 100, mod this segment by 100, draw.
+            increment = scoreArray[i] / 100;
+            scoreArray[i] %= 100;
+            if (scoreArray[i] == 0) {
+                set_vram_byte(score_pointers[i*2], 0x01);
+                set_vram_byte(score_pointers[i*2 + 1], 0x01);
+            } else {
+                set_vram_byte(score_pointers[i*2], (scoreArray[i]/10) + 1);
+                set_vram_byte(score_pointers[i*2 + 1], (scoreArray[i]%10) + 1);
+            }
+        }
+        // If we've not overflown, then draw the new number.
+        else {
+            set_vram_byte(score_pointers[i*2], (scoreArray[i]/10) + 1);
+            set_vram_byte(score_pointers[i*2 + 1], (scoreArray[i]%10) + 1);
+            break;
+        }
+    }
+
+    // Increment high score.
+    if (scoreArray[0] > highScoreArray[0]) {
+        for (uint8_t i = 0; i < 6; i++) {
+            set_vram_byte(score_pointers[i+6], get_vram_byte(score_pointers[i]));
+        }
+        highScoreArray[0] = scoreArray[0];
+        highScoreArray[1] = scoreArray[1];
+        highScoreArray[2] = scoreArray[2];
+    } else if (scoreArray[1] > highScoreArray[1] && highScoreArray[0] == scoreArray[0]) {
+        for (uint8_t i = 2; i < 6; i++) {
+            set_vram_byte(score_pointers[i+6], get_vram_byte(score_pointers[i]));
+        }
+        highScoreArray[1] = scoreArray[1];
+        highScoreArray[2] = scoreArray[2];
+    } else if (scoreArray[2] > highScoreArray[2] && highScoreArray[0] == scoreArray[0] && highScoreArray[1] == scoreArray[1]) {
+        for (uint8_t i = 4; i < 6; i++) {
+            set_vram_byte(score_pointers[i+6], get_vram_byte(score_pointers[i]));
+        }
+        highScoreArray[2] = scoreArray[2];
+    }
+}
 
 uint8_t getBeneaths(Dave *dave, uint8_t index)
 {
@@ -61,7 +125,7 @@ void gravity(Dave *dave)
         {
             /* Two, so check the tile further right.
                If it's a solid tile, then we can't fall! (0x00, 0x01, 0x0C, 0x0D, 0x0E are all solid) */
-            if (!(0x01 < tilesBeneath[1] && (tilesBeneath[1] < 0x0C || tilesBeneath[1] == 0x0F)))
+            if (!(0x27 < tilesBeneath[1] && (tilesBeneath[1] < 0x32 || tilesBeneath[1] == 0x35)))
             {
                 cantFall = 1;
             }
@@ -69,7 +133,7 @@ void gravity(Dave *dave)
 
         /* Check the main tile.
            If it's a solid tile, then we can't fall! (0x00, 0x01, 0x0C, 0x0D, 0x0E are all solid) */
-        if (!(0x01 < tilesBeneath[0] && (tilesBeneath[0] < 0x0C || tilesBeneath[0] == 0x0F)))
+        if (!(0x27 < tilesBeneath[0] && (tilesBeneath[0] < 0x32 || tilesBeneath[0] == 0x35)))
         {
             cantFall = 1;
         }
@@ -82,7 +146,7 @@ void gravity(Dave *dave)
             {
                 /* Two, so check the tile further right.
                    If it's a solid tile, then we can't fall! (0x00, 0x01, 0x0C, 0x0D, 0x0E are all solid) */
-                if (!(0x01 < tilesBeneath[3] && (tilesBeneath[3] < 0x0C || tilesBeneath[3] == 0x0F)))
+                if (!(0x27 < tilesBeneath[3] && (tilesBeneath[3] < 0x32 || tilesBeneath[3] == 0x35)))
                 {
                     cantFall = 2;
                 }
@@ -90,7 +154,7 @@ void gravity(Dave *dave)
 
             /* Check the main tile.
                If it's a solid tile, then we can't fall! (0x00, 0x01, 0x0C, 0x0D, 0x0E are all solid) */
-            if (!(0x01 < tilesBeneath[2] && (tilesBeneath[2] < 0x0C || tilesBeneath[2] == 0x0F)))
+            if (!(0x27 < tilesBeneath[2] && (tilesBeneath[2] < 0x32 || tilesBeneath[2] == 0x35)))
             {
                 cantFall = 2;
             }
@@ -153,18 +217,18 @@ void jump(Dave *dave, uint8_t incrementJump, uint8_t startJump)
         if (dave->x != 0)
         {
             /* Two tiles, check further right one. */
-            if (!((0x01 < tilesBeneath[1] && tilesBeneath[1] < 0x0C) || tilesBeneath[1] == 0x0F))
+            if (!((0x27 < tilesBeneath[1] && tilesBeneath[1] < 0x32) || tilesBeneath[1] == 0x35))
             {
                 isOnGround = 1;
                 /* Check main tile now. */
             }
-            else if (!((0x01 < tilesBeneath[0] && tilesBeneath[0] < 0x0C) || tilesBeneath[0] == 0x0F))
+            else if (!((0x27 < tilesBeneath[0] && tilesBeneath[0] < 0x32) || tilesBeneath[0] == 0x35))
             {
                 isOnGround = 1;
             }
             /* Check only the main tile. */
         }
-        else if (!((0x01 < tilesBeneath[0] && tilesBeneath[0] < 0x0C) || tilesBeneath[0] == 0x0F))
+        else if (!((0x27 < tilesBeneath[0] && tilesBeneath[0] < 0x32) || tilesBeneath[0] == 0x35))
         {
             isOnGround = 1;
         }
@@ -238,13 +302,26 @@ void jump(Dave *dave, uint8_t incrementJump, uint8_t startJump)
 
 int main(void)
 {
+    font_t hudFont;
     uint8_t joypadVal = 0;
     uint8_t jumpHeld = 0;
     uint8_t jumpPressed = 0;
     uint8_t jumpFreq = 0;
+
+    /* Set default state. Equivalent to 1UP 000000-HI 000000 */
+    const unsigned char DEFAULT_WINDOW_MAP[] = {
+        0x02, 0x1F, 0x1A, 0x00, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
+        0x2D,
+        0x12, 0x13, 0x00, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01};
+
+    /* 32-bit integers are too big, so we'll use multiple 8-bit instead. Three segments
+       of two digits, specifically, to speed up drawing at the expense of memory usage.
+    */
+    uint8_t score[] = {0, 0, 0};
+    uint8_t highScore[] = {0, 0, 0};
     Dave dave;
 
-    /* Initialize hardware registers. */
+    /* Initialize hardware registers for sound. */
     NR52_REG = 0x80;
     NR50_REG = 0x77;
     NR51_REG = 0xFF;
@@ -259,8 +336,13 @@ int main(void)
     dave.physTimer = 0;
     dave.jumpForce = 0;
 
-    /* Initialize background tile information. */
-    set_bkg_data(0, 20, BackgroundTiles1);
+    /* Initialize text tile data. */
+    font_init();
+    hudFont = font_load(font_min);
+    font_set(hudFont);
+
+    /* Initialize background tile information. Offset to avoid overwriting font. */
+    set_bkg_data(38, 15, BackgroundTiles1);
     set_bkg_tiles(0, 0, BackgroundTileMapWidth, BackgroundTileMapHeight, BackgroundTileMap);
 
     /* Initialize sprite tile information. */
@@ -269,13 +351,26 @@ int main(void)
     set_sprite_tile(0, 0);
     move_sprite(0, 88, 57);
 
-    /* Initialize music. 
-    CRITICAL {
+    /* Initialize window data (text, for score display) */
+    set_win_tiles(0, 0, 20, 1, DEFAULT_WINDOW_MAP);
+    move_win(7, 136);
+
+    /* Initialize window pointers for faster writing later. Possible y value should be 1? No clue. */
+    for (uint8_t i = 0; i < 6; i++)
+    {
+        score_pointers[i] = get_win_xy_addr(i + 4, 0);
+        score_pointers[i + 6] = get_win_xy_addr(i + 14, 0);
+    }
+
+    /* Initialize music. */
+    CRITICAL
+    {
         hUGE_init(&dave_bgm);
         add_VBL(hUGE_dosound);
-    } */
+    }
     /* Initialize display. */
     SHOW_BKG;
+    SHOW_WIN;
     SHOW_SPRITES;
     DISPLAY_ON;
 
@@ -286,6 +381,11 @@ int main(void)
         /* Handle input. */
         if (joypadVal = joypad())
         {
+            if (joypadVal & J_B)
+            {
+                incrementAndDrawScore(score, highScore, 1);
+            }
+
             if (joypadVal & J_A)
             {
                 if (jumpHeld == 0)
@@ -298,7 +398,6 @@ int main(void)
                 {
                     jumpPressed = 0;
                 }
-
             }
             else
             {
@@ -394,6 +493,10 @@ int main(void)
                 }
                 dave.mvtTimer = (dave.mvtTimer + 1) % 12;
             }
+            else
+            {
+                set_sprite_tile(0, 0);
+            }
         }
         else
         {
@@ -405,7 +508,6 @@ int main(void)
 
         /* Handle jumping and gravity. */
         jump(&dave, jumpHeld, jumpPressed);
-
     }
     return 1;
 }
